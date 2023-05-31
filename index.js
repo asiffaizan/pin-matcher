55df8f1718d7291393f7727ad969c2e076db4435
asiffaizan <asiffaizan0077@gmail.com>
2023-05-31 11:21:58 +0600

    display

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// pin verifications
 let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('buttons'));
 buttons.map(button =>{
@@ -5,6 +6,27 @@ buttons.map(button =>{
         console.log(e);
         console.log(e.target);
         console.log(e.target.innerText);
-        
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0,-1);
+                }
+                break;
+            case'Enter ↵':
+                display.innerText = "submitted";
+            default:
+                display.innerText += e.target.innerText;
+                break;
+        }
     })
 })
+
+// pin generator
+ let generatorBtn = document.getElementById('generate');
+ let pinDisplay = document.getElementById('pinDisplay');
+ generatorBtn.addEventListener('click',()=>{
+   pinDisplay.innerText = Math.floor(Math.random() * 9000 + 1000);
+ })
