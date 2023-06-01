d2324d875f18710944b2a482e1e50cce2757883a
asiffaizan <asiffaizan0077@gmail.com>
2023-06-01 18:42:45 +0600

    project completed

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// pin generator
+let generatorBtn = document.getElementById('generate');
+let pinDisplay = document.getElementById('pinDisplay');
+generatorBtn.addEventListener('click',()=>{
+  pinDisplay.innerText = Math.floor(Math.random() * 9000 + 1000);
+})
+
 // pin verifications
+let tryLeft = document.getElementById('tryCount').innerText;
 let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('buttons'));
 buttons.map(button =>{
     button.addEventListener('click',(e)=>{
-        console.log(e);
-        console.log(e.target);
-        console.log(e.target.innerText);
+        // console.log(e.target.innerText);
         switch (e.target.innerText) {
             case 'C':
                 display.innerText = '';
@@ -16,7 +22,22 @@ buttons.map(button =>{
                 }
                 break;
             case'Enter ↵':
-                display.innerText = "submitted";
+                if(display.innerText == pinDisplay.innerText){
+                    display.innerText = '';
+                    alert('Pin Matched... Secret door is opening for you');
+                }else{
+                    display.innerText = '';
+                    alert('Pin didn\'t match. Please try again');
+                    if(tryLeft > 1){
+                        decries('tryCount');
+                    }else{
+                        decries('tryCount');
+                        disabled('generate');
+                        disabled('equal');
+                    }
+                    console.log(tryLeft);
+                }
+                break;
             default:
                 display.innerText += e.target.innerText;
                 break;
@@ -24,9 +45,15 @@ buttons.map(button =>{
     })
 })
 
-// pin generator
- let generatorBtn = document.getElementById('generate');
- let pinDisplay = document.getElementById('pinDisplay');
- generatorBtn.addEventListener('click',()=>{
-   pinDisplay.innerText = Math.floor(Math.random() * 9000 + 1000);
- })
+
+ //pin matcher
+ function decries(id){
+    let newCount = tryLeft -= 1;
+    document.getElementById(id).innerText = newCount;
+ }
+ function disabled(id) {
+    let button = document.getElementById(id);
+    button.disabled = true;
+    button.style.cursor = 'not-allowed';
+    button.title = 'You have no more try left. Please try again later';
+ }
